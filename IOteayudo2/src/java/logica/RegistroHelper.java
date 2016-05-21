3f8e569d4e7162a72f6dfe01c75677614e77d4b9
davidnieto23 <davidnieto23@gmail.com>
2016-05-21 17:34:26 -0500

    Registro Corregido Para La Segunda Iteracion

--- IOteayudo2/src/java/logica/RegistroHelper.java
+++ IOteayudo2/src/java/logica/RegistroHelper.java
@@ -34,8 +34,9 @@ public class RegistroHelper {
      */
     public void registraUsuarioAlumno(Usuario usuario) throws TransactionException {
         Transaction tx = session.beginTransaction();
-        Alumno alumno = new Alumno(usuario, new Date());
-        alumno.setFecNac(new Date());
+        
+        Alumno alumno = new Alumno(usuario, new Date(94,10,10));
+        //alumno.setFecNac(new Date());
         session.persist(usuario);
         session.persist(alumno);
         tx.commit();
@@ -50,7 +51,8 @@ public class RegistroHelper {
      * error durante la transacción.
      */
     public void registraUsuarioTutor(Usuario usuario) throws TransactionException {
-        Transaction tx = session.beginTransaction();
+        Transaction tx = session.beginTransaction();      
+       
         Tutor tutor = new Tutor(usuario);
         session.persist(usuario);
         session.persist(tutor);
