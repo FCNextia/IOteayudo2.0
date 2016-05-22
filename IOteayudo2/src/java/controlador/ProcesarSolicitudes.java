2684ed177c607b21ff99516646af13fea82596e3
manu-msr <manu@ciencias.unam.mx>
2016-05-22 03:31:05 -0500

    Avances sobre enviar solicitud.

---
+++ IOteayudo2/src/java/controlador/ProcesarSolicitudes.java
@@ -0,0 +1,96 @@
+package controlador;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import logica.EnviaSolicitudHelper;
+import modelo.Alumno;
+import modelo.Asesoria;
+import modelo.Materia;
+import modelo.Solicitud;
+import modelo.Tutor;
+
+/**
+ * Clase para envíar, aceptar y rechazar solicitudes.
+ * @author Manuel Soto Romero
+ */
+@ManagedBean
+@RequestScoped
+public class ProcesarSolicitudes {
+    
+    /* Materia que se está pidiendo. */
+    private String materia;
+    /* Tutor elegido. */
+    private int tutor;
+    /* Obtiene información de la aplicación. */
+    private final FacesContext faceContext;
+    /* Para mostrar mensajes en la vista. */
+    private FacesMessage message;
+    /* Lógica para enviar solicitudes. */
+    private EnviaSolicitudHelper esh;
+    
+    /**
+     * Constructor por omisión.
+     */
+    public ProcesarSolicitudes() {
+        faceContext = FacesContext.getCurrentInstance();
+        esh = new EnviaSolicitudHelper();
+    }
+    
+    /**
+     * Método que envía una solicitud.
+     * @return String perfil del alumno.
+     */
+    public String enviaSolicitud() {
+        Solicitud solicitud = new Solicitud();
+        solicitud.setIdSolicitud(1729);
+        solicitud.setAsesoria(new Asesoria());
+        solicitud.setEstado('e');
+        CerrarSesion cs = new CerrarSesion();
+        String mail = cs.getCorreo();
+        int idMateria = obtenID("Matemáticas");
+        esh.guardaSolicitud(solicitud, mail, tutor, idMateria);
+        return "perfilalumno";
+    }
+    
+    private int obtenID(String s) {
+        switch(s) {
+            case "Matemáticas":
+                return 1;
+            case "Física":
+                return 2;
+            case "Química":
+                return 3;
+            case "Biología":
+                return 4;
+            case "Informática":
+                return 5;
+            case "Historia":
+                return 6;
+            case "Geografía":
+                return 7;
+            case "Literatura":
+                return 8;
+            case "Derecho":
+                return 9;
+        }
+        return -1;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(int tutor) {
+        this.tutor = tutor;
+    }
+}
---
+++ IOteayudo2/src/java/logica/EnviaSolicitudHelper.java
@@ -0,0 +1,55 @@
+package logica;
+
+import java.util.Date;
+import modelo.Alumno;
+import modelo.Asesoria;
+import modelo.Materia;
+import modelo.Solicitud;
+import modelo.Tutor;
+import modelo.Usuario;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.TransactionException;
+
+/**
+ * Clase para enviar solicitudes.
+ * @author Manuel Soto Romero
+ */
+public class EnviaSolicitudHelper {
+    
+    /* Para comunicarnos con la base. */
+    private final Session session;
+    
+    /**
+     * Constructor por omisión.
+     */
+    public EnviaSolicitudHelper() {
+        session = HibernateUtil.getSessionFactory().getCurrentSession();
+    }
+    
+    /**
+     * Método encargado de guardar la solicitud.
+     * @param solicitud Solicitud a guardar.
+     * @throws TransactionException En caso de error durante la transacción.
+     */
+    public void guardaSolicitud(Solicitud solicitud, String mail, int idTutor, int idMateria) throws TransactionException {
+        Transaction tx = session.beginTransaction();
+        Query p = session.getNamedQuery("BuscaPorCorreo").setString("correoUsuario", mail);
+        Usuario u = (Usuario)p.uniqueResult();
+        Query q = session.getNamedQuery("BuscaAlumnoPorID").setInteger("idUsuario", u.getIdUsuario());
+        Alumno a = (Alumno)q.uniqueResult();
+        Query s = session.getNamedQuery("BuscaTutorPorID").setInteger("idUsuario", 2);
+        Tutor t = (Tutor)s.uniqueResult();
+        Query r = session.getNamedQuery("BuscaMateriaPorID").setInteger("idMateria", idMateria);
+        Materia m = (Materia)r.uniqueResult();
+        solicitud.setAlumno(a);
+        solicitud.setTutor(t);
+        solicitud.setMateria(m);
+        Asesoria as = new Asesoria(1729, 260, new Date(), "Pendiente");
+        solicitud.setAsesoria(as);
+        session.persist(solicitud.getAsesoria());
+        session.persist(solicitud);
+        tx.commit();
+    }
+}
